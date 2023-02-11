0537347f579df128ad973ad765fc2c7ccbb23780
harseille <joonhabaak@gmail.com>
2023-02-11 17:02:26 +0900

    update package.json

--- api/Todo.ts
+++ api/Todo.ts
@@ -2,12 +2,10 @@ import axios from 'axios';
 
 export const fetchTodo = async () => {
   const response = await axios.get(`/api/todo`);
-
   return response;
 };
 
 export const addTodo = async (newTodo: any) => {
   const response = await axios.post(`/api/todo`, newTodo);
-
   return response;
 };
